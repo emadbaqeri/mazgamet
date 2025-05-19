ea22f50a3e13bac1c44f68aebecf0418a6c5642a
emad <emadbaqeri@outlook.com>
2025-05-19 11:56:33 +0330

    Add string representation to AST nodes

--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -3,6 +3,7 @@ use std::any::Any;
 
 pub trait Node {
     fn token_literal(&self) -> String;
+    fn as_string(&self) -> String;
 }
 
 pub trait Statement: Node + Any {
@@ -26,6 +27,14 @@ impl Node for Program {
             String::new()
         }
     }
+
+    fn as_string(&self) -> String {
+        self.statements
+            .iter()
+            .map(|statement| statement.as_string())
+            .collect::<Vec<String>>()
+            .join("")
+    }
 }
 
 pub struct Identifier {
@@ -40,6 +49,10 @@ impl Expression for Identifier {
 impl Node for Identifier {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
+    }
+
+    fn as_string(&self) -> String {
+        self.value.clone()
     }
 }
 
@@ -62,6 +75,25 @@ impl Node for LetStatement {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
     }
+
+    fn as_string(&self) -> String {
+        let mut output = String::new();
+
+        if let Some(name) = &self.name {
+            output.push_str("let ");
+            output.push_str(&name.as_string());
+            output.push_str(" = ");
+
+            if let Some(value) = &self.value {
+                output.push_str(&value.as_string());
+            }
+        } else {
+            output.push_str("let _ = _");
+        }
+
+        output.push(';');
+        output
+    }
 }
 
 pub struct ReturnStatement {
@@ -73,6 +105,10 @@ impl Node for ReturnStatement {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
     }
+
+    fn as_string(&self) -> String {
+        "return ...;".to_string()
+    }
 }
 
 impl Statement for ReturnStatement {
@@ -80,5 +116,76 @@ impl Statement for ReturnStatement {
 
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+pub struct ExpressionStatement {
+    token: Token,
+    expression: Option<Box<dyn Expression>>,
+}
+
+impl Statement for ExpressionStatement {
+    fn statement_node(&self) {}
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn as_string(&self) -> String {
+        if let Some(expression) = &self.expression {
+            expression.as_string()
+        } else {
+            String::new()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lexer::TokenType;
+
+    #[test]
+    fn test_string() {
+        // Create an identifier for the variable name
+        let name = Identifier {
+            token: Token {
+                token_type: TokenType::Identifier,
+                literal: "myVar".to_string(),
+            },
+            value: "myVar".to_string(),
+        };
+
+        // Create an identifier for the value
+        let value = Identifier {
+            token: Token {
+                token_type: TokenType::Identifier,
+                literal: "anotherVar".to_string(),
+            },
+            value: "anotherVar".to_string(),
+        };
+
+        // Create a let statement
+        let let_stmt = LetStatement {
+            token: Token {
+                token_type: TokenType::Let,
+                literal: "let".to_string(),
+            },
+            name: Some(Box::new(name)),
+            value: Some(Box::new(value)),
+        };
+
+        // Create a program with the let statement
+        let program = Program {
+            statements: vec![Box::new(let_stmt)],
+        };
+
+        // Test the string representation
+        assert_eq!(program.as_string(), "let myVar = anotherVar;");
     }
 }
